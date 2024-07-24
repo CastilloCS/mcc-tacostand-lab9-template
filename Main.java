f62f161dd1625a56b6fc1b6f54c1bd455e06c13f
Stephen Cerruti <scerruti@miracosta.edu>
2024-07-24 11:46:34 -0700

    Merged in new updates from main for testing.

--- Main.java
+++ Main.java
@@ -18,6 +18,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		//DECLARATION + INITIALIZATION SECTION
+		TacoStand.initialize();
 
 		//INPUT + CALCULATION + OUTPUT SECTION
 		TacoStand.addTotalFunds(20);
